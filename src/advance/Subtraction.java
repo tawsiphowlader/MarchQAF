2d41893aaac850c68254c4546fffcfffef909f87
tawsiphowlader <tawsiphowlader@localhost>
2022-05-19 19:45:07 -0400

    first commit https://github.com/tawsiphowlader/MarchQAF.git

--- src/advance/Subtraction.java
+++ src/advance/Subtraction.java
@@ -5,11 +5,11 @@ public class Subtraction extends Addition {
 	void sub(int a, int b) {
 		int s = a- b;
 		System.out.println(s);	
-								//when we need a inheritance? when we need a access a method from a different class. "extence"
-								// why we need inheritance? so can child method can access parents method
-	}							//aaccess modifier -1.private--only accesible with class. 2. public- accessiblle within project 
-								//level. 3. default- accessible withing package. 4. protected - access within package but 
-								//accessible outside page if the class is inheritated.
+		//when we need a inheritance? when we need a access a method from a different class. "extence"
+		// why we need inheritance? so can child method can access parents method
+	}	//access modifier -1.private--only accessible with class. 2. public- accessible within project 
+		//level. 3. default- accessible within package. 4. protected - access within package but 
+		//accessible outside page if the class is inheritated.
 						
 	
 	
